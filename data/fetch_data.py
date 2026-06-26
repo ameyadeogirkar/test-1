316a929ef70307ab389453ed0a7a3c2259a57f0f
AMEYA DEOGIRKAR <69581470+ameyadeogirkar@users.noreply.github.com>
2026-06-26 21:44:08 +0530

    Handle MultiIndex columns in fetch_stock_data
    Added handling for MultiIndex columns in fetched DataFrame.

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -4,19 +4,22 @@ from datetime import datetime, timedelta
 def fetch_stock_data(ticker):
 
     end = datetime.today()
-
     start = end - timedelta(days=5 * 365)
 
     stock = yf.Ticker(ticker)
 
     df = yf.download(
-    ticker,
-    start=start,
-    end=end,
-    progress=False,
-    auto_adjust=True,
-    group_by="column"
-)
+        ticker,
+        start=start,
+        end=end,
+        progress=False,
+        auto_adjust=True,
+        group_by="column"
+    )
+
+    if isinstance(df.columns, pd.MultiIndex):
+        df.columns = df.columns.get_level_values(0)
+
     info = stock.info
 
     return df, info
