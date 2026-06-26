451f33d1ff3cd124685a80ec21c05ab9c47aee84
AMEYA DEOGIRKAR <69581470+ameyadeogirkar@users.noreply.github.com>
2026-06-26 21:42:56 +0530

    Modify fetch_stock_data to group data by column
    Added 'group_by' parameter to download function.

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -10,13 +10,13 @@ def fetch_stock_data(ticker):
     stock = yf.Ticker(ticker)
 
     df = yf.download(
-        ticker,
-        start=start,
-        end=end,
-        progress=False,
-        auto_adjust=True
-    )
-
+    ticker,
+    start=start,
+    end=end,
+    progress=False,
+    auto_adjust=True,
+    group_by="column"
+)
     info = stock.info
 
     return df, info
